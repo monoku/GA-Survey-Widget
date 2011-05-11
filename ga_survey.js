7df4f07d0d962f0cfe82d3ef9f3e8c2db6084275
Jeronimo Osorio <pixelhorror@gmail.com>
2011-05-11 18:10:38 -0500

    Removed mootools depencies. Must fix the click event

--- ga_survey.js
+++ ga_survey.js
@@ -4,50 +4,79 @@
 //By @PixelHorror
 //For Monoku Labs
 
+function check_resources(){
+    interval_id = setInterval(Main,50);
+}
 
-window.addEvent('load', function(){
-    //Main Questions Array
-    var questions = {
-        q1: {question:"Cual es tu color favorito?",question_short:"Color",answers:["rojo","verde","azul"]},
-        q2: {question:"Cual es Genero?",question_short:"genero",answers:["Masculino","Femenino"]},
-        q3: {question:"Cual sabor de helado favorito?",question_short:"sabores",answers:["Fresa","Vainilla","Mora","Atun"]}
-    };
-    //Common Variables
-    var counter = 1;
-    var selected_question;
-    var is_found = false;
+function stop_check(){
+    clearInterval(interval_id);
+}
 
-    while(!is_found && counter < 6)
-    {
-        var pageTracker = _gat._getTrackerByName();
-        var customVar = pageTracker._getVisitorCustomVar(counter);
-        if (typeof(customVar) === "undefined")
+check_resources();
+
+function Main(){
+    if (_gat) {
+        //Main Questions Array
+        var questions = {
+            q1: {question:"Cual es tu color favorito?",question_short:"Color",answers:["rojo","verde","azul"]},
+            q2: {question:"Cual es Genero?",question_short:"genero",answers:["Masculino","Femenino"]},
+            q3: {question:"Cual sabor de helado favorito?",question_short:"sabores",answers:["Fresa","Vainilla","Mora","Atun"]}
+        };
+        //Common Variables
+        var counter = 1;
+        var selected_question;
+        var is_found = false;
+
+        while(!is_found && counter < 6)
         {
-            selected_question = counter;
-            is_found = true;
+            var pageTracker = _gat._getTrackerByName();
+            var customVar = pageTracker._getVisitorCustomVar(counter);
+            if (typeof(customVar) === "undefined")
+            {
+                selected_question = counter;
+                is_found = true;
+            }
+            counter++;
         }
-        counter++;
-    }
 
-    if(counter === 6) {
-        function generate_html(index){
-            var current_question = questions["q"+index];
+            function generate_html(index){
+                var current_question = questions["q"+index];
 
-            new Element('p',{html:current_question.question}).inject($('widget_question'));
-            for (var i=0; i < current_question.answers.length; i++)
-                new Element('li',{html:current_question.answers[i]}).inject($('widget_answers'));
+                var question_text = document.createElement("p");
+                question_text.innerHTML = current_question.question;
+                var question_spot = document.getElementById('widget_question');
+                var answers_spot= document.getElementById('widget_answers');
+
+                question_spot.appendChild(question_text);
+
+                for (var i=0; i < current_question.answers.length; i++)
+                {
+                    var answers_text = document.createElement("li");
+                    answers_text.innerHTML = current_question.answers[i];
+                    answers_spot.appendChild(answers_text);
+                }
+            }
+
+        if(counter < 6) {
+            generate_html(selected_question);
         }
+
+        /*var answer_list = document.getElementById("widget_answers");*/
+
+        //for (var i=0;i < 2 ; i++)
+        //{
+            //answer_list[i].addEventListener("click",push_to_analytics);
+        //}
+
+            function push_to_analytics()
+            {
+                _gaq.push(['_setCustomVar',
+                selected_question,                   
+                questions["q"+selected_question].question_short,
+                this.innerHTML, 
+                1  
+                ]);
+            }
+        stop_check();
     }
-
-    generate_html(selected_question);
-
-    $$('#widget_answers li').addEvent('click', function() {
-
-        _gaq.push(['_setCustomVar',
-            selected_question,                   
-            questions["q"+selected_question].question_short,
-            this.innerHTML, 
-            1  
-            ]);
-    });
-});
+}
