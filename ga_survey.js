c8e3f734e5890ed64412d51182eab4947b0d1a12
Jeronimo Osorio <pixelhorror@gmail.com>
2011-05-23 09:34:15 -0500

    added transitions, fixed FF bug with order of events, removed some debug code

--- ga_survey.js
+++ ga_survey.js
@@ -82,7 +82,6 @@ function Main(){
             parent_div.appendChild(answers_ul);
             parent_div.appendChild(info_div);
         }
-
         
         //Injects the style
         function inject_style(){
@@ -93,6 +92,33 @@ function Main(){
             style_element.innerHTML = "type='text/css'>html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,font,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td{margin:0;padding:0;border:0;outline:0;font-weight:inherit;font-style:inherit;font-size:100%;font-family:inherit;vertical-align:baseline}body{line-height:1;color:black;background:white}ol,ul{list-style:none}table{border-collapse:separate;border-spacing:0;vertical-align:middle}caption,th,td{text-align:left;font-weight:normal;vertical-align:middle}q,q:before,q:after,blockquote:before,img{border:none}#widget_container{display:inline-block;position:relative;border:1px solid #c3c3c3;-moz-border-radius:5px;-webkit-border-radius:5px;-o-border-radius:5px;-ms-border-radius:5px;-khtml-border-radius:5px;border-radius:5px;font-family:Verdana, Arial;font-size:12px;color:#5e5e5e;width:250px}#widget_container #widget_question{padding:10px}#widget_container #widget_answers{padding:10px}#widget_container #widget_answers li{-moz-border-radius:5px;-webkit-border-radius:5px;-o-border-radius:5px;-ms-border-radius:5px;-khtml-border-radius:5px;border-radius:5px;-moz-box-shadow:#c3c3c3 2px 2px 2px 1px 1px 5px 0;-webkit-box-shadow:#c3c3c3 2px 2px 2px 1px 1px 5px 0;-o-box-shadow:#c3c3c3 2px 2px 2px 1px 1px 5px 0;box-shadow:#c3c3c3 2px 2px 2px 1px 1px 5px 0;border:1px solid #c3c3c3;color:#009ada;padding:8px;margin:5px 0}#widget_container #widget_answers li:hover{color:white;background:#009ada;border:1px solid #3D7F7F;cursor:pointer}#widget_container #widget_info{background:#dddddd}#widget_container .hidden{display:none}#widget_container .thanks{padding:10px;color:#009ADA;font-weight:bold;font-size:15px}";
             head_element.appendChild(style_element);
         }
+        function bind_events() {
+
+            var answer_list = document.getElementById("widget_answers");
+
+            for (var i=1;i <= answer_list.childElementCount; i++)
+            {
+                answer_list.childNodes[i].addEventListener("click",push_to_analytics,false);
+            } 
+        }
+
+        function push_to_analytics()
+        {
+            _gaq.push(['_setCustomVar',
+                    selected_question,                   
+                    questions["q"+selected_question].question_short,
+                    this.innerHTML, 
+                    1  
+                    ]);
+            var container = document.getElementById("widget_question");
+            var second_container = document.getElementById("widget_answers");
+
+            var info = document.getElementById("widget_thanks");
+
+            container.style.display = "none";
+            second_container.style.display = "none";
+            info.style.display = "block";
+        }
 
         if(is_found) {
             generate_html(selected_question);
@@ -100,29 +126,10 @@ function Main(){
             inject_style();
             bind_events();
         }
-
-
-        function bind_events() {
-
-            var answer_list = document.getElementById("widget_answers");
-
-            for (var i=1;i <= answer_list.childElementCount; i++)
-            {
-                console.log(answer_list.childNodes[i]);
-                //This line fucks shit up like no tomorrow, it doesn't let the code stop checking.
-                answer_list.childNodes[i].addEventListener("click",push_to_analytics);
-            } 
+        else {
+            console.log("All done, bro");
         }
 
-        function push_to_analytics()
-        {
-            _gaq.push(['_setCustomVar',
-            selected_question,                   
-            questions["q"+selected_question].question_short,
-            this.innerHTML, 
-            1  
-            ]);
-        }
         stop_check();
     }
 }
